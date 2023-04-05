54568f1fc6a14fd37446e6fff0fc225ee2fa9497
oktavianinganim <127914193+oktavianinganim@users.noreply.github.com>
2023-04-05 12:07:01 +0800

    Add files via upload

---
+++ oktaviani2201010432/latihan02.php
@@ -0,0 +1,57 @@
+<?php
+
+    $usr = "";
+    $ps ="";
+    $eusr = "";
+    $eps = "";
+    if(isset($_POST["txUSER"])){
+        $usr = $_POST["txUSER"];
+        $ps =$_POST["txPASSWORD"];
+
+        if($usr == ""){
+$eusr = "<div style='color: red;font-size: 9px;'>User Name masih kosong</div>";
+        
+        }
+        if($ps == "" ){
+            $eps = "<div style='color: red;font-size: 9px;'>User Name masih kosong</div>";
+                    
+                    }
+        
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form menggunakan methode POST</title>
+</head>
+<body>
+    
+    <form method="POST" action="latihan02.php">
+
+    <div>
+        user name
+        <input type="text" name="txUSER">
+        <?=$eusr;?>
+    </div>    
+    <div>
+        password
+        <input type="password" name="txPASSWORD">
+        <?$eps;?>
+    </div>    
+
+    <div>
+        <button type="submit"> login </button>
+    </div>
+</form>
+<div>
+        <pre>
+            user name: <?=$usr?>
+            password : <?=$ps?>
+    </pre > 
+   </div>  
+</body>
+</html>
